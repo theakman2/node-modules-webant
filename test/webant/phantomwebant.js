dda6cd856881494516c80c447ae1bf7edceefaf0
theakman2 <theakman2@hotmail.com>
2013-11-09 14:12:52 +0000

    'window not defined' bug-fix.

--- test/webant/phantomwebant.js
+++ test/webant/phantomwebant.js
@@ -3,7 +3,7 @@ var page = require("webpage").create();
 page.open("./build/index.html",function(status){
 	setTimeout(function(){
 		var g = page.evaluate(function(){
-			return window.__global;
+			return __global;
 		});
 		console.log(JSON.stringify(g));
 		phantom.exit();
